acfc1feafa186cc9d6dd0a288b3f881dfc909005
lalrom_76 <romain_thierry2002@hotmail.fr>
2021-11-16 16:13:21 +0100

    header connexion css

--- inc/header.php
+++ inc/header.php
@@ -12,6 +12,27 @@
     <link href="https://fonts.googleapis.com/css2?family=Poiret+One&family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
 
     <!-- STYLE.CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
+<header class="header">
+    <div class="wrap0">
+        <nav>
+            <ul>
+                <li><a href="#">accueil</a></li>
+                <li><a href="#">vaccins</a></li>
+            </ul>
+        </nav>
+        <div class="connection">
+            <div class="coimg">
+                <img src="../maquette/icons/user.svg" alt="user">
+            </div>
+            <div class="colink">
+                <ul>
+                    <li><a href="#">Inscription</a></li>
+                    <li><a href="#">Connexion</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
