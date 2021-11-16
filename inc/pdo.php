f1d550a9c4a6890708cbed233dc1752bfed70876
Maxence-BOSSIN <maxbossin25@gmail.com>
2021-11-16 20:51:34 +0100

    variable fichier pdo

--- inc/pdo.php
+++ inc/pdo.php
@@ -1,7 +1,10 @@
 <?php
-
+$serveur  = 'localhost';
+$bdd      = 'psv';
+$username = 'root';
+$password = '';
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=nfs8', "root", "root", array(
+    $pdo = new PDO('mysql:host='.$serveur.';dbname='.$bdd.'', ''.$username.'', ''.$password.'', array(
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
