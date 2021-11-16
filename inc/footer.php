c0069feb61c2bb09a7626f1436f979f866487e0f
anaiscrenier <anais000@hotmail.fr>
2021-11-16 16:53:23 +0100

    footer.scss et footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,20 @@
+<footer id="footer">
+   <div class="wrap0">
+       <nav>
+           <ul>
+               <li><a href="#">ACCUEIL-</a></li>
+               <li><a href="#">VACCINS-</a></li>
+               <li><a href="#">CONTACT-</a></li>
+               <li><a href="#">MENTIONS LÉGALES</a></li>
+           </ul>
+       </nav>
+   </div>
+</footer>
+
+
+
+
+
 
 </body>
 </html>
