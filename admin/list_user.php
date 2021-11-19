9f73986f3fdab3b32a859ad7e1cc3768e2dcfeed
lalrom_76 <romain_thierry2002@hotmail.fr>
2021-11-19 11:16:25 +0100

    list person

--- admin/list_user.php
+++ admin/list_user.php
@@ -1,5 +1,13 @@
 <?php
-
+require ('../inc/pdo.php');
+require ('inc/request.php');
+require ('../inc/fonction.php');
+global $pdo;
+$sql = "SELECT id,nom,prenom,age,email,pseudo FROM psv_user";
+$query = $pdo->prepare($sql);
+$query->execute();
+$users = $query->fetchAll();
+$carnet = getVaccinsUser(2);
 include('inc/header_back.php');
 ?>
 
@@ -28,64 +36,45 @@ include('inc/header_back.php');
             <!-- ============================================================== -->
             <div class="row">
                 <div class="col-12">
-<<<<<<< HEAD
                     <div class="card">
                         <div class="card-body">
                             <div class="table-responsive">
                                 <table class="table">
                                     <thead>
                                     <tr>
-                                        <th>#</th>
-                                        <th>First Name</th>
-                                        <th>Last Name</th>
-                                        <th>Username</th>
+                                        <th>Numéro</th>
+                                        <th>Pseudo</th>
+                                        <th>Prenom</th>
+                                        <th>Nom</th>
+                                        <th>Age</th>
+                                        <th>Email</th>
+                                        <th>Vaccin</th>
                                     </tr>
                                     </thead>
-                                    <tbody>
+                                    <tbody><?php
+                                    foreach ($users as $user){ ?>
                                     <tr>
-                                        <td>1</td>
-                                        <td>Deshmukh</td>
-                                        <td>Prohaska</td>
-                                        <td>@Genelia</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Deshmukh</td>
-                                        <td>Gaylord</td>
-                                        <td>@Ritesh</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>Sanghani</td>
-                                        <td>Gusikowski</td>
-                                        <td>@Govinda</td>
-                                    </tr>
-                                    <tr>
-                                        <td>4</td>
-                                        <td>Roshan</td>
-                                        <td>Rogahn</td>
-                                        <td>@Hritik</td>
-                                    </tr>
-                                    <tr>
-                                        <td>5</td>
-                                        <td>Joshi</td>
-                                        <td>Hickle</td>
-                                        <td>@Maruti</td>
-                                    </tr>
-                                    <tr>
-                                        <td>6</td>
-                                        <td>Nigam</td>
-                                        <td>Eichmann</td>
-                                        <td>@Sonu</td>
-                                    </tr>
+                                        <td><?=$user['id']?></td>
+                                        <td><?=$user['pseudo']?></td>
+                                        <td><?=$user['prenom']?></td>
+                                        <td><?=$user['nom']?></td>
+                                        <td><?=$user['age']?></td>
+                                        <td><?=$user['email']?></td>
+                                        <td><?php if (empty($carnet['id_vaccin'])){
+                                            echo 'ok';
+                                            }else{
+                                            echo 'ko';
+                                            }?></td>
+                                    </tr> <?php
+                                    }
+                                    ?>
                                     </tbody>
                                 </table>
+                                <?=debug($users);?>
+                                <?=debug($carnet);?>
                             </div>
                         </div>
                     </div>
-=======
-
->>>>>>> e6feb3e2367d352dabad586ade86d4503fce55a1
                 </div>
             </div>
         </div>
