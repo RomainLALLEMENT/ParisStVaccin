bbf3e3689078fc73f4bfa715179fd312ffcc7d49
lalrom_76 <romain_thierry2002@hotmail.fr>
2021-11-19 09:38:51 +0100

    modif pdo

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@ include ('inc/header_back.php');
             <!-- ============================================================== -->
             <div class="row page-titles">
                 <div class="col-md-5 align-self-center">
-                    <h3 class="text-themecolor">Blank Page</h3>
+                    <h3 class="text-themecolor">Blank Page</h3>x
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
                         <li class="breadcrumb-item active">Blank Page</li>
