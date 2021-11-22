bfd10e9c4eb790e7c93a76acf0e3cedcbfa418db
brietalexis <alexis.briet2003@gmail.com>
2021-11-22 12:15:09 +0100

    update font

--- inc/header.php
+++ inc/header.php
@@ -7,7 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Accueil - ParisStVaccin</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poiret+One&family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
     <!-- STYLE.CSS -->
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
