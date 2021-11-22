eeec97c02b35fac26a72c8b7e74190aeeb13d114
brietalexis <alexis.briet2003@gmail.com>
2021-11-22 12:45:29 +0100

    refonte footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,11 @@
 <footer id="footer">
-    <div class="wrap0">
+
+    <div class="wrap2">
+        <div class="logo_footer">
+            <a href="index.php">
+                <img src="assets/img/footer/logo_footer.png">
+            </a>
+        </div>
         <nav class="navigation">
             <ul>
                 <li><a href="#">Accueil</a></li>
@@ -7,18 +13,13 @@
                 <li><a href="#">Contact</a></li>
                 <li><a href="#">Mentions légales</a></li>
             </ul>
+            <p class="copyright">&copy; Copyright 2021 - Tous les droits sont réservés.</p>
         </nav>
-        <div class="separator"></div>
-            <div class="logo_footer">
-                <img src="assets/img/footer/logo_footer.png">
-            </div>
-        <div class="separator"></div>
         <ul class="social">
             <li><a href="#"><i class="fab fa-facebook"></i></a></li>
             <li><a href="#"><i class="fab fa-twitter"></i></a></li>
             <li><a href="#"><i class="fab fa-instagram"></i></a></li>
         </ul>
-        <p class="copyright">&copy; Copyright 2021 - Tous les droits sont réservés.</p>
     </div>
 </footer>
 
