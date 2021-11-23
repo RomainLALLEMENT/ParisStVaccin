74d76d4093cb6fe8b1be830c5a60da7507ca8d26
brietalexis <alexis.briet2003@gmail.com>
2021-11-23 11:10:42 +0100

    ajouter vaccin admin

--- admin/add_vaccin.php
+++ admin/add_vaccin.php
@@ -10,6 +10,7 @@ if (!empty($_POST['submitted'])) {
     //faille xss
     $libelle = cleanXss('libelle');
     $temps_rappel = cleanXss('temps_rappel');
+    $country = cleanXss('country');
     $obligatoire = cleanXss('obligatoire');
     $description = cleanXss('description');
     $laboratoire = cleanXss('laboratoire');
@@ -17,21 +18,24 @@ if (!empty($_POST['submitted'])) {
     //validation
     $errors = textValidation($errors,$libelle,'libelle', 2,255);
     $errors = textValidation($errors,$temps_rappel,'temps_rappel', 1,3);
-    $errors = textValidation($errors,$obligatoire,'obligatoire', 0,1);
+    $errors = textValidation($errors,$country,'country', 1,255);
     $errors = textValidation($errors,$description,'description', 5,500);
     $errors = textValidation($errors,$laboratoire,'laboratoire', 2,150);
 
-    //requete sql
 
-    $sql= "INSERT INTO  psv_vaccin ( libelle, temps_rappel, pays, obligatoire, description, Laboratoire) 
-            VALUES (:libelle, :temps_rappel, :obligatoire, :description, :laboratoire)";
-    $query = $pdo->prepare($sql);
-    $query->bindValue(':libelle',$libelle, PDO::PARAM_STR);
-    $query->bindValue(':temps_rappel',$temps_rappel, PDO::PARAM_INT);
-    $query->bindValue(':obligatoire',$obligatoire,PDO::PARAM_STR);
-    $query->bindValue(':description',$description, PDO::PARAM_STR);
-    $query->bindValue(':laboratoire',$laboratoire,PDO::PARAM_STR);
-    $query->execute();
+    if (count($errors) == 0 ) {
+        //requete sql
+
+        $sql= "INSERT INTO  psv_vaccin ( libelle, temps_rappel, pays, obligatoire, description, Laboratoire) 
+            VALUES (:libelle, :temps_rappel, :pays, :obligatoire, :description, :laboratoire)";
+        $query = $pdo->prepare($sql);
+        $query->bindValue(':libelle',$libelle, PDO::PARAM_STR);
+        $query->bindValue(':temps_rappel',$temps_rappel, PDO::PARAM_INT);
+        $query->bindValue(':obligatoire',$obligatoire,PDO::PARAM_STR);
+        $query->bindValue(':description',$description, PDO::PARAM_STR);
+        $query->bindValue(':laboratoire',$laboratoire,PDO::PARAM_STR);
+        $query->execute();
+    }
 }
 
 
@@ -39,6 +43,7 @@ include('inc/header_back.php');
 ?>
 
     <div class="page-wrapper">
+    <?=debug($_POST) ?>
         <!-- ============================================================== -->
         <!-- Container fluid  -->
         <!-- ============================================================== -->
@@ -77,41 +82,64 @@ include('inc/header_back.php');
                                 <div class="form-group">
                                     <label for="temps_rappel" class="col-md-12">Temps rappel</label>
                                     <div class="col-md-12">
-                                        <input type="number" name="temps_rappel" id="temps_rappel" placeholder="" value="<?= recupInputValue('temps_rappel') ?>" class="form-control form-control-line">
+                                        <input type="number" name="temps_rappel" id="temps_rappel" placeholder="Nombre de mois" min="3" max="100" value="<?= recupInputValue('temps_rappel') ?>" class="form-control form-control-line">
                                         <?= viewError($errors,'temps_rappel') ?>
                                     </div>
                                 </div>
 
-                                <label class="col-sm-12">Select Country</label>
+                                <?php
+
+                                    $countrys = [
+                                            'fr' => 'France',
+                                            'gb' => 'Angleterre',
+                                            'de' => 'Allemagne',
+                                            'it' => 'Italie',
+                                            'es' => 'Espagne'
+                                    ];
+
+                                ?>
+
+                                <label class="col-sm-12">Sélectionnez un pays</label>
                                 <div class="form-group">
                                     <div class="col-sm-12">
-                                        <select class="form-control form-control-line">
-                                            <option>London</option>
-                                            <option>India</option>
-                                            <option>Usa</option>
-                                            <option>Canada</option>
-                                            <option>Thailand</option>
+                                        <select class="form-control form-control-line" name="country" id="v">
+                                            <option value="">__sélectionnez__</option>
+                                            <?php foreach ($countrys as $key => $country) { ?>
+                                            <?php if (!empty($_POST['country']) && $_POST['country'] == $key ) { ?>
+                                                <option value="<?= $key ?>" selected><?= $country ?></option>
+                                            <?php } else { ?>
+                                                <option value="<?= $key ?>"><?= $country ?></option>
+                                                <?php } ?>
+                                            <?php } ?>
                                         </select>
+                                        <?= viewError($errors,'country') ?>
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="obligatoire" class="col-md-12">Obligatoire</label>
+                                    <span>Vaccin obligatoire ?</span>
                                     <div class="col-sm-12">
-                                        <input type="checkbox" name="obligatoire" id="obligatoire" value="Vaccin obligatoire">
-                                        <?= viewError($errors,'obligatoire') ?>
+                                        <div>
+                                            <input type="radio" id="obligatoire_false" name="obligatoire" value="0"
+                                                   checked>
+                                            <label for="obligatoire_false">Non</label>
+                                        </div>
+                                        <div>
+                                            <input type="radio" id="obligatoire_true" name="obligatoire" value="1">
+                                            <label for="obligatoire_true">Oui</label>
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label for="description" class="col-md-12">Description</label>
                                     <div class="col-sm-12">
-                                        <textarea name="description" id="description"><?= recupInputValue('description')?></textarea>
+                                        <textarea name="description" id="description" class="form-control form-control-line" style="height: 200px; padding: 1rem"><?= recupInputValue('description')?></textarea>
                                         <?= viewError($errors,'description') ?>
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label for="laboratoire" class="col-md-12">Laboratoire</label>
                                     <div class="col-sm-12">
-                                        <input type="text" name="laboratoire" id="laboratoire" value="<?= recupInputValue('laboratoire')?>">
+                                        <input type="text" name="laboratoire" id="laboratoire" value="<?= recupInputValue('laboratoire')?>" class="form-control form-control-line">
                                         <?= viewError($errors,'laboratoire') ?>
                                     </div>
                                 </div>
