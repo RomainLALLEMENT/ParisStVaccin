41c129a48999b988839b3b6e0c4e46752fd125df
lalrom_76 <romain_thierry2002@hotmail.fr>
2021-11-24 12:34:06 +0100

    ajout barre de recherche vaccin

--- vaccins.php
+++ vaccins.php
@@ -4,47 +4,54 @@ require ('inc/pdo.php');
 require ('inc/fonction.php');
 require ('inc/request.php');
 include ('inc/header.php');
+
+// faire un formulaire
+//Quand il est soumis vérifier faille xss
+// Si pas de faille appeller la fonction
+//afficher les résultats
+$vaccins = getAfficherVaccin();
+if (!empty($_POST['submitted'])) {
+    $recherche = cleanXss('submitted');
+    $recherche = getRechercheVaccin($_POST['submitted']);
+   }
+if (!empty($recherche)){$vaccins = $recherche;}
 ?>
 
-    <section id="home">
-        <div class="wrap0">
-            <div class="title_absolute uppercase">
-                <h2 class="text-shadow">Vaccinez</h2>
-                <h2 class="text-shadow">Vous !</h2>
-                <?php if (empty($_SESSION)) { ?>
-                    <a href="login.php">Connexion</a>
-                <?php } else { ?>
-                    <a href="listVaccinsUser.php">Mon carnet</a>
-                <?php } ?>
-            </div>
+<section id="home">
+    <div class="wrap0">
+        <div class="title_absolute uppercase">
+            <h2 class="text-shadow">Vaccinez</h2>
+            <h2 class="text-shadow">Vous !</h2>
+            <?php if (empty($_SESSION)) { ?>
+                <a href="login.php">Connexion</a>
+            <?php } else { ?>
+                <a href="listVaccinsUser.php">Mon carnet</a>
+            <?php } ?>
         </div>
-    </section>
+    </div>
+</section>
+
 <section id="vaccins">
     <div class="wrap1">
-    <div class="vrecher">
-        <label for="recherche site">Rechercher des vaccins</label>
-        <input type="search" id="site-recherche" placeholder="tapez votre recherche">
-    </div>
-    <?php
-    $sql = "SELECT * FROM psv_vaccin ORDER BY RAND() LIMIT 4";
-    $query = $pdo->prepare($sql);
-    $query->execute();
-    $vaccins=$query->fetchAll();
-
-    ?><div class="vaccins"> <?php
-            foreach ($vaccins as $vaccin){  ?>
+        <div class="vrecher">
+            <form action="" method="post">
+                <label for="recherche site">Rechercher des vaccins</label>
+                <input type="search" id="site-recherche" placeholder="tapez votre recherche" name="submitted">
+            </form>
+            <?php
+            if (!empty($recherche))echo '<p> Résultats pour : \''.$_POST['submitted'].'\' ';
+            ?>
+        </div>
+        <div class="vaccins"><?php
+            foreach ($vaccins as $vaccin){?>
                 <div class="vaccin">
                     <div><div class="vimg"></div></div>
                     <h2><?= $vaccin['libelle'];?></h2>
                     <p><?= $vaccin['description'];?></p>
                 </div>
-                <?php } ?>
+            <?php } ?>
         </div>
     </div>
 </section>
-    <br>
-    <br>
-    <br>
-    <br>
 <?php
 include ('inc/footer.php');
